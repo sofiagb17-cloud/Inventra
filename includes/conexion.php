8585a20e4908823e6a1c3858c22796201740f930
sofiagb17-cloud <lesslysofiagb17@gmail.com>
2026-05-14 16:42:10 -0500

    Update conexion.php

--- includes/conexion.php
+++ includes/conexion.php
@@ -6,10 +6,10 @@
 // Si no existen, usa los valores por defecto de Railway
 
 $host     = getenv('DB_HOST') ?: 'shuttle.proxy.rlwy.net';
-$port     = getenv('DB_PORT') ?: '16824';
-$dbname   = getenv('DB_NAME') ?: 'railway';
+$port     = getenv('DB_PORT') ?: '5432';
+$dbname   = getenv('DB_NAME') ?: 'inventario_db';
 $user     = getenv('DB_USER') ?: 'postgres';
-$password = getenv('DB_PASSWORD') ?: 'txccDihzEGeuqPYsPIYTVtfJOxCeKkWX';
+$password = getenv('DB_PASSWORD') ?: '47460';
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
